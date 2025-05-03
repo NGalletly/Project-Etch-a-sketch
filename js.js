3755bb3214b143320214e19adc721b81067a50f1
Neville Galletly <207318973+NGalletly@users.noreply.github.com>
2025-05-03 07:05:52 +0700

    Edit Added Sketch logic

--- js.js
+++ js.js
@@ -12,11 +12,68 @@ function createGrid(num) {
   }
 }
 
-let columns = prompt("How many rows?");
+columns = 16;
 let cellCount = Math.pow(columns, 2);
-console.log(cellCount);
-console.log(columns);
 
 container.style.setProperty("--columns", columns);
+
+// boxes.classList.add("color");
+
+//DRAWING LOGIC
+
+let boxes = document.querySelectorAll(".box");
+
+let mouseDown = false;
+
+container.addEventListener("mousedown", () => {
+  mouseDown = true;
+  console.log("mousedown = true");
+});
+
+container.addEventListener("mouseup", () => {
+  mouseDown = false;
+  console.log("mouseup = false");
+});
+
+boxes.forEach((box) => {
+  box.addEventListener("mousedown", () => {
+    box.style.background = "black";
+  });
+});
+
+boxes.forEach((box) => {
+  box.addEventListener("mousemove", () => {
+    if (mouseDown === true) {
+      box.style.background = "black";
+    }
+  });
+});
+
+//BUTTONS LOGIC
+
+// <button class="gridInputBtn">Grid Size</button>
+// <button class="colorSwapBtn">Colour Swap</button>
+// <button class="clearGrid">Clear Grid</button>
+// let columns = prompt("How many rows?");
+
+let gridInputBtn = document.querySelector(".gridInputBtn");
+let colorSwapBtn = document.querySelector(".colorSwapBtn");
+let clearGridBtn = document.querySelector(".clearGridBtn");
+
+gridInputBtn.addEventListener("click", () => {
+  console.log("grid input pressed");
+  columns = prompt("How many rows?");
+  console.log(columns + " button");
+  cellCount = Math.pow(columns, 2);
+  createGrid(cellCount);
+});
+
+colorSwapBtn.addEventListener("click", () => {
+  console.log("colour swap pressed");
+});
+
+clearGridBtn.addEventListener("click", () => {
+  console.log("clear grid pressed");
+});
 
 createGrid(cellCount);
